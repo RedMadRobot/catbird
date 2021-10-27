da0922187e97eec3e102ea0b63809b8e935984e1
Daniil Subbotin <d.subbotin@opends.tech>
2021-10-27 16:54:22 +0300

    Fix RedirectMiddleware

--- Sources/CatbirdApp/AppConfiguration.swift
+++ Sources/CatbirdApp/AppConfiguration.swift
@@ -27,20 +27,20 @@ extension AppConfiguration {
 
     /// Detect application configuration.
     public static func detect(
-        from enviroment: [String: String] = ProcessInfo.processInfo.environment
+        from environment: [String: String] = ProcessInfo.processInfo.environment
     ) throws -> AppConfiguration {
 
         let mocksDirectory = try { () throws -> URL in
-            let path = enviroment["CATBIRD_MOCKS_DIR", default: sourceDir]
+            let path = environment["CATBIRD_MOCKS_DIR", default: sourceDir]
             guard let url = URL(string: path) else {
                 fatalError("Invalid URL CATBIRD_MOCKS_DIR=\(path)")
             }
             return url
         }()
 
-        let maxBodySize = enviroment["CATBIRD_MAX_BODY_SIZE", default: "50mb"]
+        let maxBodySize = environment["CATBIRD_MAX_BODY_SIZE", default: "50mb"]
 
-        if let path = enviroment["CATBIRD_PROXY_URL"], let url = URL(string: path) {
+        if let path = environment["CATBIRD_PROXY_URL"], let url = URL(string: path) {
             return AppConfiguration(mode: .write(url), mocksDirectory: mocksDirectory, maxBodySize: maxBodySize)
         }
         return AppConfiguration(mode: .read, mocksDirectory: mocksDirectory, maxBodySize: maxBodySize)
