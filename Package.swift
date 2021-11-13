99aa83204048074b50419335a55d685f31c954a5
Daniil Subbotin <d.subbotin@opends.tech>
2021-11-13 16:05:30 +0300

    Extract catbird executable product from Package.swift into CatbirdApp folder with it is own Package.swift

--- Package.swift
+++ Package.swift
@@ -4,40 +4,15 @@ import PackageDescription
 let package = Package(
     name: "Catbird",
     platforms: [
-       .macOS(.v10_15)
+        .iOS(.v12),
+        .macOS(.v10_15)
     ],
     products: [
-        .library(name: "CatbirdAPI", targets: ["CatbirdAPI"]),
-        .executable(name: "catbird", targets: ["CatbirdRun"]),
-    ],
-    dependencies: [
-        // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
-
-        // Templete engine
-        .package(url: "https://github.com/stencilproject/Stencil.git", from: "0.14.0"),
-
-        // macOS system logger
-        .package(url: "https://github.com/Alexander-Ignition/OSLogging", from: "1.0.0"),
+        .library(name: "CatbirdAPI", targets: ["CatbirdAPI"])
     ],
     targets: [
         // Common API
         .target(name: "CatbirdAPI"),
-        .testTarget(name: "CatbirdAPITests", dependencies: ["CatbirdAPI"]),
-
-        // Web Server
-        .target(name: "CatbirdApp", dependencies: [
-            .target(name: "CatbirdAPI"),
-            .product(name: "Vapor", package: "vapor"),
-            .product(name: "Stencil", package: "Stencil"),
-            .product(name: "OSLogging", package: "OSLogging"),
-        ]),
-        .testTarget(name: "CatbirdAppTests", dependencies: [
-            .target(name: "CatbirdApp"),
-            .product(name: "XCTVapor", package: "vapor"),
-        ]),
-
-        // CLI
-        .target(name: "CatbirdRun", dependencies: ["CatbirdApp"]),
+        .testTarget(name: "CatbirdAPITests", dependencies: ["CatbirdAPI"])
     ]
 )
---
+++ Packages/CatbirdApp/Package.swift
@@ -0,0 +1,41 @@
+// swift-tools-version:5.2
+import PackageDescription
+
+let package = Package(
+    name: "CatbirdApp",
+    platforms: [
+       .macOS(.v10_15)
+    ],
+    products: [
+        .executable(name: "catbird", targets: ["CatbirdRun"]),
+    ],
+    dependencies: [
+        // 💧 A server-side Swift web framework.
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
+
+        // Templete engine
+        .package(url: "https://github.com/stencilproject/Stencil.git", from: "0.14.0"),
+
+        // macOS system logger
+        .package(url: "https://github.com/Alexander-Ignition/OSLogging", from: "1.0.0"),
+
+        // CatbirdAPI
+        .package(path: "../../")
+    ],
+    targets: [
+        // Web Server
+        .target(name: "CatbirdApp", dependencies: [
+            .product(name: "CatbirdAPI", package: "catbird"),
+            .product(name: "Vapor", package: "vapor"),
+            .product(name: "Stencil", package: "Stencil"),
+            .product(name: "OSLogging", package: "OSLogging"),
+        ]),
+        .testTarget(name: "CatbirdAppTests", dependencies: [
+            .target(name: "CatbirdApp"),
+            .product(name: "XCTVapor", package: "vapor"),
+        ]),
+
+        // CLI
+        .target(name: "CatbirdRun", dependencies: ["CatbirdApp"]),
+    ]
+)
