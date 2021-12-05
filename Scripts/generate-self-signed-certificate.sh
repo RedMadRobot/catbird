106fea1b19227d77719154d4c29300daacc692af
Daniil Subbotin <d.subbotin@opends.tech>
2021-12-05 14:48:39 +0300

    Convert RSA Key from .pem to .key format

--- Scripts/generate-self-signed-certificate.sh
+++ Scripts/generate-self-signed-certificate.sh
@@ -15,6 +15,11 @@ read -s -p  "Password: " password
 # Generate RSA Key
 openssl genrsa -aes256 -passout pass:"$password" -out key.pem 2048
 
+# Convert RSA Key from .pem to .key format
+openssl rsa -outform der -in key.pem -out cert.key -passin pass:"$password"
+
+echo "Key created: cert.key"
+
 # Generate the self-signed certificate and private key
 openssl req -x509 -new -nodes -passin pass:"$password" -config "$CONFIG_FILE" -key key.pem -sha256 -extensions v3_ca -days 365 -out root-ca.pem
 
